666bb0bbb802e497fdc8f27ae49f4e77aa9dfc5c
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-02 18:59:54 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

---
+++ m4/p3/longest_substring.py
@@ -0,0 +1,22 @@
+"""largest"""
+def main():
+    """gives largest substring"""
+    str_ = input()
+    tstr_ = ""
+    for i, item in enumerate(str_):
+        flag_ = 0
+        tstr2_ = ""
+        tt_ = item
+        ji_ = i
+        while flag_ != 1 and ji_ != len(str_)
+            if ord(tt_) <= ord(str_[ji_]):
+                tt_ = str_[ji_]
+                tstr2_ += str_[ji_]
+            else:
+                flag_ = 1
+            ji_ += 1
+        if len(tstr2_) > len(tstr_):
+            tstr_ = tstr2_
+    print(tstr_)
+if __name__ == "__main__":
+    main()

393f3718c2f93fec8dc184583823db939317e07e
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-02 17:05:28 +0530

    " -> 5 of 5 passed. pylint: 8.18/10 "

--- m4/p2/bob_counter.py
+++ m4/p2/bob_counter.py
@@ -7,7 +7,7 @@ def main():
     str_ = 'bob'
     cou = 0
     for i in range(len(stri_)-2):
-        if str2 == str1[i]+str[i+1]+str[i+2]:
+        if str_ == stri_[i]+stri_[i+1]+stri_[i+2]:
             cou = cou + 1
     print(cou)
 if __name__== "__main__":

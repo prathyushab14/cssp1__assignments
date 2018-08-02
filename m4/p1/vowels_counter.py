1acd9b3dde96dff0e8328e73553902c43adafd44
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-02 14:50:42 +0530

    " -> 0 of 4 passed. pylint: 0.00/10 "

--- m4/p1/vowels_counter.py
+++ m4/p1/vowels_counter.py
@@ -7,13 +7,13 @@
 
 def main():
     """vowel"""
-    s_ = input()
+    str_ = input()
     # the input string is in s
     # remove pass and start your code here
-    c_ = 0
+    count_ = 0
     for letter_ in s:
         if letter_ == 'a' or letter_ == 'e' or letter_ == 'i' or letter_ == 'o' or letter_ == 'u':
-            c_ = c_ + 1
-    print(c_)
+            count_ = count_ + 1
+    print(count_)
 if __name__== "__main__":
-	main()
+    main()

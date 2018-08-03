8aecb30c5576fa40a93fe1f82e13689aaaffe932
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-03 14:54:28 +0530

    " -> 0 of 4 passed. pylint: -1.67/10 "

--- m5/p1/perfect_cube.py
+++ m5/p1/perfect_cube.py
@@ -1,15 +1,14 @@
 """cube"""
 def main():
-	"""input is captured in s"""
-    x_1 = int(input())
-    for guess_1 in range(abs(x_1)+1):
-        if guess_1**3 >= abs(x_1):
-            break
-        if guess_1**3 != abs(x_1):
-            print(x_1, 'is not a perfect cube')
-        else:
-            if x_1 < 0:
-                guess_1 = -guess_1
-            print('cube root of' + str(x_1) + ' is ' + str(guess_1))
+	x_1 = int(input())
+	for guess_1 in range(abs(x_1)+1):
+		if guess_1**3 >= abs(x_1):
+			break
+	if guess_1**3 != abs(x_1):
+		print(x_1, 'is not a perfect cube')
+	else:
+		if x_1 < 0:
+			guess_1 = -guess_1
+	print('cube root of' + str(x_1) + ' is ' + str(guess_1))
 if __name__ == "__main__":
-    main()
+	main()

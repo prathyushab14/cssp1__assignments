3ef4e74c33d0fc8c3600039d89238ab192570b7f
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-04 17:23:56 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- m5/p2/square_root.py
+++ m5/p2/square_root.py
@@ -1,5 +1,5 @@
 """square root"""
-# Write a python program to find the square root of the given number 
+# Write a python program to find the square root of the given number
 # using approximation method
 def main():
     """main"""
--- m5/p3/square_root_bisection.py
+++ m5/p3/square_root_bisection.py
@@ -1,9 +1,23 @@
-# Write a python program to find the square root of the given number 
-# using Bi-section method
-
+"""square root"""
+# Write a python program to find the square root of the given number
+# using approximation method
 def main():
 	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+	"""square root"""
+	epsilon = 0.01
+	x_q = int(input())
+    epsilon_ = 0.01
+    numuesses_ = 0
+    low_ = 0.0
+    high_ = x_q
+    ans_ = (high_ + low_)/2.0
+    while abs(ans_**2 - x_q) >= epsilon_:
+        numuesses_ += 1
+        if ans_**2 < x_q:
+            low_ = ans_
+        else:
+            high_ = ans_
+        ans_ = (high_ + low_)/2.0
+    print(str(ans_))
+if __name__ == "__main__":
+    main()

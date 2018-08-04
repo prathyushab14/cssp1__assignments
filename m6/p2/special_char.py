8e4f9e3247f3fe6bc1af8eef58d0a1cabc051146
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-04 11:22:59 +0530

    " -> 0 of 7 passed. pylint: 5.00/10 "

--- m6/p2/special_char.py
+++ m6/p2/special_char.py
@@ -8,10 +8,9 @@ def main():
     Read string from the input, store it in variable str_input.
     '''
     str_input = input()
-    s1 = ' '
     for i in str_input:
     	if (i == '!' or '@' or '#' or '$' or '%' or '^' or '&' or '*'):
-    		i = s1
+    		i = ' '
     print(i)
 if __name__ == "__main__":
     main()

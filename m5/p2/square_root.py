567b1413ce60dbca84aa11e0bf841428fd514d30
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-04 17:02:16 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- m5/p2/square_root.py
+++ m5/p2/square_root.py
@@ -3,7 +3,19 @@
 
 def main():
 	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+	 """square"""
+    squ_ = int(input())
+    eps_ = 0.01
+    guess_ = 0.0
+    cla_ = 0
+    increment_ = 0.1
+    num_guesses = 0
+    while abs(guess_**2 - squ_) >= eps_:
+        guess_ += increment_
+        num_guesses += 1
+    if abs(guess_**2 - squ_) >= eps_:
+        cla_ = cla_+1
+    else:
+        print(str(guess_))
+if __name__ == "__main__":
+    main()

cfc165b7943984408444c21c04b0b3f5ef071c82
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-04 10:13:56 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- m6/p1/fizz_buzz.py
+++ m6/p1/fizz_buzz.py
@@ -10,14 +10,14 @@ def main():
     '''
     num = int(input())
     for i in range(1,num+1):
-        if(i%3 == 0 and i%5 ==0):
+        if(i%3 == 0 and i%5 == 0):
             print("Fizz")
             print("Buzz")
         elif(i%5 == 0):
             print("Buzz")
         elif(i%3 == 0):
             print("Fizz")
-        else:
+        else
             print(i)
 if __name__ == "__main__":
     main()

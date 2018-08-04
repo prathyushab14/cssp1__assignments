10ce547414b5fea16c4efd194438f65a98107033
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-04 10:48:05 +0530

    " -> 0 of 7 passed. pylint: 3.89/10 "

--- m6/p3/digit_product.py
+++ m6/p3/digit_product.py
@@ -17,6 +17,7 @@ def main():
         product = - (product*l)
     if(int_input == 0):
         print(0)
+        break
     while(int_input >= 1):
         l = int_input % 10
         int_input = int_input // 10

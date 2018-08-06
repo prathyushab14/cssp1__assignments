7e68e11c6e28addcf76d55ca3f2e58b2a1cb0d6f
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-06 21:49:45 +0530

    " -> 8 of 10 passed. pylint: 10.00/10 "

--- m7/Functions - Assignment-3/assignment3.py
+++ m7/Functions - Assignment-3/assignment3.py
@@ -1,20 +1,3 @@
-# Functions - Assignment-3 - Using Bisection Search to Make the Program Faster
-
-# You'll notice that in Problem 2, your monthly payment had to be a multiple of $10. Why did we make it that way? You can try running
-# your code locally so that the payment can be any dollar and cent amount (in other words, the monthly payment is a multiple of $0.01).
-# Does your code still work? It should, but you may notice that your code runs more slowly, especially in cases with very large balances
-# and interest rates. (Note: when your code is running on our servers, there are limits on the amount of computing time each submission
-# is allowed, so your observations from running this experiment on the grading system might be limited to an error message complaining
-# about too much time taken.)
-
-# Well then, how can we calculate a more accurate fixed monthly payment than we did in Problem 2 without running into the problem of slow
-# code?
-# We can make this program run faster using a technique introduced in lecture - bisection search!
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-
 """ass3"""
 def paying_debt_off_in_a_year(balance, annual_interest_rate):
     """bisec"""
@@ -47,4 +30,3 @@ def main():
     print("Lowest Payment: " + str(round(paying_debt_off_in_a_year(data[0], data[1]), 2)))
 if __name__ == "__main__":
     main()
-    

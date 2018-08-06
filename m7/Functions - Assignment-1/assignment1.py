342189039ee4dd57df2be2374c77a632379f55a1
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-06 17:28:12 +0530

    " -> 0 of 10 passed. pylint: -1.11/10 "

--- m7/Functions - Assignment-1/assignment1.py
+++ m7/Functions - Assignment-1/assignment1.py
@@ -26,16 +26,16 @@
 
 
 def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
-	temp_b = balance
-	no_of_months = 12
-	while no_of_months >= 1:
+    temp_b = balance
+    no_of_months = 12
+    while no_of_months >= 1:
         monthly_interest_rate = annualInterestRate / 12.0
-	    minimum_monthly_payment = monthlyPaymentRate * temp_b
-	    monthly_unpaid_balance = temp_b - minimum_monthly_payment
-	    updated_balance_each_month = monthly_unpaid_balance + (monthly_unpaid_balance * monthly_interest_rate)
-	    temp_b = updated_balance_each_month
-	    no_of_months -= 1
-	print("remaining balance = ", temp_b)
+        minimum_monthly_payment = monthlyPaymentRate * temp_b
+        monthly_unpaid_balance = temp_b - minimum_monthly_payment
+        updated_balance_each_month = monthly_unpaid_balance + (monthly_unpaid_balance * monthly_interest_rate)
+        temp_b = updated_balance_each_month
+        no_of_months -= 1
+    print("remaining balance = ", temp_b)
 def main():
 	data = input()
 	data = data.split(' ')

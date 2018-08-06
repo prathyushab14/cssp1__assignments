19fe5f108546e8fa1924b9bed61b222b8f9bc62a
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-06 10:56:25 +0530

    quadratic

--- m7/Functions - Eval Quadratic Exercise/eval_quadratic.py
+++ m7/Functions - Eval Quadratic Exercise/eval_quadratic.py
@@ -1,8 +1,8 @@
-# Exercise: eval quadratic
+"""# Exercise: eval quadratic
 
 # Write a Python function, evalQuadratic(a, b, c, x), that returns the value of the quadratic a . x 2 + b . x + c
 
-# This function takes in four numbers and returns a single number.
+# This function takes in four numbers and returns a single number."""
 
 
 def evalQuadratic(a, b, c, x):

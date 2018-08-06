fedefe5fd65c927f6c2e95e225f0273ce68c5917
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-06 17:56:53 +0530

    " -> 10 of 10 passed. pylint: 6.67/10 "

--- m7/Functions - Assignment-1/assignment1.py
+++ m7/Functions - Assignment-1/assignment1.py
@@ -1,11 +1,11 @@
 """bal"""
-def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
+def payingDebtOffInAYear(balance, annual_interest_rate, monthlypaymentrate):
     """payment"""
     temp_b = balance
     no_of_months = 12
     while no_of_months >= 1:
-        monthly_interest_rate = annualInterestRate / 12.0
-        minimum_monthly_payment = monthlyPaymentRate * temp_b
+        monthly_interest_rate = annual_interest_rate / 12.0
+        minimum_monthly_payment = monthlypaymentrate * temp_b
         monthly_unpaid_balance = temp_b - minimum_monthly_payment
         updated_balance_each_month = monthly_unpaid_balance + (monthly_unpaid_balance * monthly_interest_rate)
         temp_b = updated_balance_each_month

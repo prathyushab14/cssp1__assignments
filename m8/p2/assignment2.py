39e14099acaa4cfb6b2a7dbe41d8d4cdf29a30b7
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-07 16:35:51 +0530

    sum

---
+++ m8/p2/assignment2.py
@@ -0,0 +1,12 @@
+"""sum"""
+def sum_of_digits(n):
+    """sum"""
+    while n_1>0:
+        r_1 = n_1%10
+        return r_1+sum_of_digits(n_1//10)
+    return 0
+def main():
+    a_1 = input()
+    print(sum_of_digits(int(a_1)))  
+if __name__ == "__main__":
+    main()

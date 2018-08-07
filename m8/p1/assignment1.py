e0c80c8d2112a53d0fcf1b0e0cc26a6ce2cfa7b0
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-07 14:51:13 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- m8/p1/assignment1.py
+++ m8/p1/assignment1.py
@@ -1,21 +1,13 @@
-# Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
-
-# This function takes in one number and returns one number.
-
-
+"""fact"""
 def factorial(n):
     """fact"""
     if n == 0 or n == 1:
-    	return 1
+        return 1
     else:
-    	return n * factorial(n-1)
-    
-
-
+        return n * factorial(n-1)
 def main():
+	"""fact"""
     a = input()
     print(factorial(int(a)))    
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()

8a65416a11f4f2cc86bf60c26d22d6523c440879
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-07 16:37:40 +0530

    " -> 10 of 10 passed. pylint: 9.00/10 "

--- m8/p2/assignment2.py
+++ m8/p2/assignment2.py
@@ -1,12 +1,12 @@
 """sum"""
 def sum_of_digits(n_1):
     """sum"""
-    while n_1>0:
+    while n_1 > 0:
         r_1 = n_1%10
         return r_1+sum_of_digits(n_1//10)
     return 0
 def main():
     a_1 = input()
-    print(sum_of_digits(int(a_1)))  
+    print(sum_of_digits(int(a_1)))
 if __name__ == "__main__":
     main()

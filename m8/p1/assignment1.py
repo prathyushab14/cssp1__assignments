608196c2aaf6d06a937107c5e44958c2a8f3d319
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-07 14:53:16 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- m8/p1/assignment1.py
+++ m8/p1/assignment1.py
@@ -2,12 +2,13 @@
 def factorial(n):
     """fact"""
     if n == 0 or n == 1:
-        return 1
+    	return 1
     else:
-        return n * factorial(n-1)
+    	return n * factorial(n-1)
 def main():
 	"""fact"""
     a = input()
     print(factorial(int(a)))    
-if __name__ == "__main__":
+
+if __name__== "__main__":
     main()

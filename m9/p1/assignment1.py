02d2aac988ee10add090f7f740dbe0c8161d6261
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-08 18:33:01 +0530

    " -> 0 of 9 passed. pylint: 0/0 "

--- m9/p1/assignment1.py
+++ m9/p1/assignment1.py
@@ -2,8 +2,10 @@
 def is_word_guessed(secret_word, letters_guessed):
     """guess"""
     cou = 0
-    for i in range(len(secret_word)):
-        for k in range(len(letters_guessed)):
+    l1 = len(secret_word)
+    l2 = len(letters_guessed)
+    for i in range l1:
+        for k in range l2:
             if secret_word[i] == letters_guessed[k]:
                 cou += 1
     return bool(cou == len(secret_word))

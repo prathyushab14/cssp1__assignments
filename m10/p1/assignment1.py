89c7f256110789436c4e8daf1b2424017fd81c6e
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-09 17:07:21 +0530

    string

---
+++ m10/p1/assignment1.py
@@ -0,0 +1,24 @@
+"""string"""
+def get_available_letters(letters_guessed):
+    """dictionaries"""
+    import string
+    s = ''
+    key1 = list(string.ascii_lowercase)
+    val1 = key1
+    dic1 = dict(zip(key1,val1))
+    for i in letters_guessed:
+        if i in dic1.key1():
+            del dic1[i]
+    for j in dic1.key1()
+        s = s + dic[i]
+    return s
+def main():
+   """string"""
+    user_input = input()
+    user_input = user_input.split()
+    data = []
+    for char in user_input:
+        data.append(char[0])
+    print(get_available_letters(data))
+if __name__ == "__main__":
+    main()

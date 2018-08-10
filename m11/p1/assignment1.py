edb8c969c2f59c9672dd5a0bf2cb1a97e1aef6ed
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-10 16:46:19 +0530

    " -> 0 of 9 passed. pylint: 4.74/10 "

--- m11/p1/assignment1.py
+++ m11/p1/assignment1.py
@@ -23,7 +23,6 @@ def get_word_score(word, n):
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     import string
-    dict1 = {}
     length = len(word)
     if length <= n:
         for j in word:

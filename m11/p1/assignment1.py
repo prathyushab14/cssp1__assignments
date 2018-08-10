cf951ac9f7947ac50fb6ddf0dd5177bf81bd2a14
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-10 16:55:02 +0530

    " -> 9 of 9 passed. pylint: 8.42/10 "

--- m11/p1/assignment1.py
+++ m11/p1/assignment1.py
@@ -1,16 +1,16 @@
 """word"""
-def get_word_score(word, n_):
+def get_word_score(word, n1_):
     """wordscore"""
     dict1_ = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     length = len(word)
     sum = 0
-    if length <= n_:
+    if length <= n1_:
         for j in word:
             if j in dict1_.keys():
                 sum = sum + dict1_[j]
         sum = sum * length
-        if length == n_:
+        if length == n1_:
             sum = sum + 50
         return sum
     return "invalid"

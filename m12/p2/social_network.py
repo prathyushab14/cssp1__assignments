1555e4de4e9eb8adfdb737403c284283a9dfe61e
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-11 12:50:22 +0530

    " -> 0 of 2 passed. pylint: -2.61/10 "

--- m12/p2/social_network.py
+++ m12/p2/social_network.py
@@ -15,7 +15,7 @@ def follow(network, arg1, arg2):
     '''
     # remove the pass below and start writing your code
     network = {}
-    network[arg1] = list.append(arg2)
+    network[arg1] = list.append[arg2]
 
 
 

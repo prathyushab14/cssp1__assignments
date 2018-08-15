1f637dffc1b2e6f4899452702755f4e27e2b2fc2
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-15 17:43:39 +0530

    " -> 0 of 8 passed. pylint: 10.00/10 "

--- m14/CodeCampPoker/poker.py
+++ m14/CodeCampPoker/poker.py
@@ -3,96 +3,6 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
-def three_ofakind(hand):
-    dict1 = {'A': 14, 'K': 13, 'Q': 12, 'J':11, 'T':10}
-    b = []
-    for i in hand:
-        # print(i)
-        a = (i[0])
-        # print(a)
-        if a in dict1.keys():
-            a = dict1[a]
-        # print(a)
-        a = int(a)
-        # print(type(a))
-        b.append(a)
-    # print(b)
-    b.sort()
-    # print(b)
-    for k in b:
-        if k in dict1.keys():
-            b[k] = dict1(b[k])
-    suit_set = set(b)
-    allTrue = True
-    l =  len(suit_set)
-    # print(suit_set)
-    # print(l)
-    if l == 2:
-        allTrue = True
-    else:
-        allTrue = False
-    if allTrue:
-        return True
-    else:
-        return False 
-def one_pair(hand):
-    dict1 = {'A': 14, 'K': 13, 'Q': 12, 'J':11, 'T':10}
-    b = []
-    for i in hand:
-        # print(i)
-        a = (i[0])
-        # print(a)
-        if a in dict1.keys():
-            a = dict1[a]
-        # print(a)
-        a = int(a)
-        # print(type(a))
-        b.append(a)
-        # print(b)
-        b.sort()
-        # print(b)
-    for k in b:
-        if k in dict1.keys():
-            b[k] = dict1(b[k])
-    one_pair = set(b)
-    allTrue = True
-    if len(one_pair) == 4:
-        allTrue = True
-    else:
-        allTrue = False
-    if allTrue:
-        return True
-    else:
-        return False
-def two_pair(hand):
-    dict1 = {'A': 14, 'K': 13, 'Q': 12, 'J':11, 'T':10}
-    b = []
-    for i in hand:
-        # print(i)
-        a = (i[0])
-        # print(a)
-        if a in dict1.keys():
-            a = dict1[a]
-        # print(a)
-        a = int(a)
-        # print(type(a))
-        b.append(a)
-    # print(b)
-    b.sort()
-    # print(b)
-    for k in b:
-        if k in dict1.keys():
-            b[k] = dict1(b[k])
-    two_pair = set(b)
-    allTrue = True
-    if len(two_pair) <= 3:
-        allTrue = True
-    else:
-        allTrue = False
-    if allTrue:
-        return True
-    else:
-        return False
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -193,12 +103,12 @@ def hand_rank(hand):
         return 4
     if is_straight(hand):
         return 5
-    if two_pair(hand):
-        return 7
-    if three_ofakind(hand):
-        return 6
-    if one_pair(hand):
-        return 8
+    # if two_pair(hand):
+    #     return 7
+    # if three_ofakind(hand):
+    #     return 6
+    # if one_pair(hand):
+    #     return 8
     return 9
 
 def poker(hands):

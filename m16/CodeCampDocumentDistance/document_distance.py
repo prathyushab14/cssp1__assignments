4599b0c71874e0724b29e94c1b3e64231bcedc3e
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-17 17:15:02 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- m16/CodeCampDocumentDistance/document_distance.py
+++ m16/CodeCampDocumentDistance/document_distance.py
@@ -2,22 +2,22 @@
     Document Distance - A detailed description is given in the PDF
 '''
 def remove_stop_words(words,stopwords):
-	words1 = []
-	words1 = words.copy()
-	for w in words:
-		if w not in stopwords and len(w)>0:
-			words1.append(w)
+    words1 = []
+    words1 = words.copy()
+    for w in words:
+        if w not in stopwords and len(w)>0:
+            words1.append(w)
     return words1
 
 def words_list(doc):
-	word = doc.lower()
-	word = word.split("")
-	words = []
-	for w in word:
-		words.append(w.strip())
-	regex = re.compile("[^a-z]")
-	words = [regex.sub(" ",w) for w in word]
-	return words
+    word = doc.lower()
+    word = word.split("")
+    words = []
+    for w in word:
+        words.append(w.strip())
+    regex = re.compile("[^a-z]")
+    words = [regex.sub(" ",w) for w in word]
+    return words
 
 
 
@@ -34,17 +34,17 @@ def similarity(dict1, dict2):
     dictionary = createDictionary(dictionary,words_1,0)
     dictionary = createDictionary(dictionary,words_2,1)
 def createDictionary(dictionary,words,index):
-	for w in word:
-		if w not in dictionary.keys()
-		    dictionary[w] = [0,0]
-		dictionary[w][index] += 1
-	return dictionary
+    for w in word:
+        if w not in dictionary.keys()
+            dictionary[w] = [0,0]
+        dictionary[w][index] += 1
+    return dictionary
 def compute(dictionary):
-	numerator = sum([v[0]*v[1] for v in dictionary.values()])
-	denominator1 = math.sqrt(sum([v[0]**2 for v in dictionary.values()]))
-	denominator2 = math.sqrt(sum([v[1]**2 for v in dictionary.values()]))
-	return numerator/(denominator1 * denominator2)
-	
+    numerator = sum([v[0]*v[1] for v in dictionary.values()])
+    denominator1 = math.sqrt(sum([v[0]**2 for v in dictionary.values()]))
+    denominator2 = math.sqrt(sum([v[1]**2 for v in dictionary.values()]))
+    return numerator/(denominator1 * denominator2)
+    
 
 
 

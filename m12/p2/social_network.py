47a51d2dd09a3c06f530258363bb43ba72fa2216
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-17 17:14:10 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- m12/p2/social_network.py
+++ m12/p2/social_network.py
@@ -17,9 +17,9 @@ def follow(network, arg1, arg2):
     if arg1 in network:
         network[arg1].append(arg2)
     else:
-    	list1 = []
-    	list1.append(arg2)
-    	network[arg1] = list1
+        list1 = []
+        list1.append(arg2)
+        network[arg1] = list1
     return network
 
 
@@ -36,7 +36,7 @@ def unfollow(network, arg1, arg2):
     '''
     # remove the pass below and start writing your code
     if arg1 in network:
-    	network[arg1].remove(arg2)
+        network[arg1].remove(arg2)
     return network
 
 def delete_person(network, arg1):
@@ -51,10 +51,10 @@ def delete_person(network, arg1):
     '''
     # remove the pass below and start writing your code
     if arg1 in network:
-    	del network[arg1]
+        del network[arg1]
     for i in network:
-    	if arg1 in network[i]:
-    		network[i].remove(arg1)
+        if arg1 in network[i]:
+            network[i].remove(arg1)
     return network
     
 
---
+++ m16/CodeCampDocumentDistance/document_distance.py
@@ -0,0 +1,71 @@
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
+def remove_stop_words(words,stopwords):
+	words1 = []
+	words1 = words.copy()
+	for w in words:
+		if w not in stopwords and len(w)>0:
+			words1.append(w)
+    return words1
+
+def words_list(doc):
+	word = doc.lower()
+	word = word.split("")
+	words = []
+	for w in word:
+		words.append(w.strip())
+	regex = re.compile("[^a-z]")
+	words = [regex.sub(" ",w) for w in word]
+	return words
+
+
+
+def similarity(dict1, dict2):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    words_1 = words_list(dict1)
+    words_2 = words_list(dict2)
+    stopwords = load_stopwords("stopwords.txt")
+    words1 = remove_stop_words(words_1,stopwords)
+    words2 = remove_stop_words(words_2,stopwords)
+    dictionary = dict()
+    dictionary = createDictionary(dictionary,words_1,0)
+    dictionary = createDictionary(dictionary,words_2,1)
+def createDictionary(dictionary,words,index):
+	for w in word:
+		if w not in dictionary.keys()
+		    dictionary[w] = [0,0]
+		dictionary[w][index] += 1
+	return dictionary
+def compute(dictionary):
+	numerator = sum([v[0]*v[1] for v in dictionary.values()])
+	denominator1 = math.sqrt(sum([v[0]**2 for v in dictionary.values()]))
+	denominator2 = math.sqrt(sum([v[1]**2 for v in dictionary.values()]))
+	return numerator/(denominator1 * denominator2)
+	
+
+
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename:
+        for line in filename:
+            stopwords[line.strip()] = 0
+    return stopwords
+
+def main():
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input()
+    input2 = input()
+
+    print(similarity(input1, input2))
+
+if __name__ == '__main__':
+    main()

d50e872ad570b34967cf727947229602462eee60
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-23 11:50:29 +0530

    " -> 0 of 8 passed. pylint: 2.40/10 "

--- m20/matrix_operations.py
+++ m20/matrix_operations.py
@@ -18,7 +18,9 @@ def add_matrix(m1, m2):
     '''
     m3=[]
     for i in range(len(m1)):
+        print(i)
         for j in range(len(m1[i])):
+            print(j)
             m3[i].append(m1[i][j] + m2[i][j])
     return m3
 

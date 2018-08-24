e8e268ead367e29b70ab6f745d23eb9ef683f2fc
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-24 10:10:18 +0530

    " -> 3 of 14 passed. pylint: 2.35/10 "

--- m21/tictac.py
+++ m21/tictac.py
@@ -15,7 +15,5 @@ def decide_winner(board):
     		return 'o'
     	if i.count('x') == 3:
     		return 'x'
-    	if i[0][0] == i[1][1] == i[2][2] == 'o':
-    		return 'o'
 if __name__ == '__main__':
 	main()

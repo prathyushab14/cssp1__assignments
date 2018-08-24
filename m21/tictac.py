55dba96cf55919a28af1bbf2ff1673a7efe6be51
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-24 09:59:45 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- m21/tictac.py
+++ m21/tictac.py
@@ -1,14 +1,17 @@
-rows, columns = 3, 3
-row = int(rows)
-column = int(columns)
-print(row)
-print(column)
-board = []
-for i in range(row):
-	lst = [i for i in input().split(" ")]
-	board.append(lst)
-print(decide_winner(board))
+def main()
+    rows, columns = 3, 3
+    row = int(rows)
+    column = int(columns)
+    print(row)
+    print(column)
+    board = []
+    for i in range(row):
+	    lst = [i for i in input().split(" ")]
+	    board.append(lst)
+    print(decide_winner(board))
 def decide_winner(board):
     for i in board:
     	if i.count(X) == 3:
     		return X
+if __name__ = '__main__':
+	main()

99ae0c91722ecc6dccab752392d6c9452d48602e
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-25 11:13:31 +0530

    " -> 0 of 4 passed. pylint: 6.00/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -5,7 +5,7 @@ each word
 
 def tokenize(string):
     lst = []
-    data = data.split("")
+    data = string.split("")
     lst = lst.append(data)
     str1 = [lst.count(word) for word in lst]
     return(dict(zip(lst,str1)))      

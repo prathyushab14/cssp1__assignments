bbcffa45646d4e32c541b620f7564658bdb33719
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-25 10:50:57 +0530

    " -> 0 of 3 passed. pylint: 5.56/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -4,7 +4,7 @@ alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
-    string = string.strip()
+    string = string.strip("\n")
     str1 = string.replace("!@#$%^&*()-<>","")
     return str1
 
